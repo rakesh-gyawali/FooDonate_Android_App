455ee09079d02ed2bed4288448208d9449adeabe
Rakesh Gyawali <41475122+rakesh-gyawali@users.noreply.github.com>
2021-01-21 16:56:57 +0545

    add rect horizontal recycler view

--- app/src/main/java/com/example/food_donation_dissertation/HomeFragment.java
+++ app/src/main/java/com/example/food_donation_dissertation/HomeFragment.java
@@ -3,16 +3,28 @@ package com.example.food_donation_dissertation;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.example.food_donation_dissertation.adapter.RowItemAdapter;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
 
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
+    ArrayList<String> mNames;
+    ArrayList<Integer> mImages;
+    ArrayList<String> mAddresses;
+    private static final String TAG = "HomeFragment";
+
+
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -37,20 +49,47 @@ public class HomeFragment extends Fragment {
             }
         };
         carouselView.setImageListener(imageListener);
-
-        CarouselView carouselView1;
-        int[] sampleImages1 = {R.drawable.poor_man, R.drawable.volunteers};
-        carouselView1 = (CarouselView) view.findViewById(R.id.carouselView1);
-        carouselView1.setPageCount(sampleImages1.length);
-        ImageListener imageListener1 = new ImageListener() {
-            @Override
-            public void setImageForPosition(int position, ImageView imageView) {
-                imageView.setImageResource(sampleImages1[position]);
-            }
-        };
-        carouselView1.setImageListener(imageListener1);
+        //-----row item-----
+          makeList();
+          initRecyclerView(view);
         return view;
+    }
+    private void makeList() {
+        mNames = new ArrayList<>();
+        mImages = new ArrayList<>();
+        mAddresses = new ArrayList<>();
 
+        mImages.add(R.drawable.charity_logo);
+        mNames.add("Rotatrot Club");
+        mAddresses.add("Chakrapath, Kathmandu");
+
+        mImages.add(R.drawable.charity_logo);
+        mNames.add("KTM Youth Club");
+        mAddresses.add("Bouddha, Kathmandu");
+
+        mImages.add(R.drawable.charity_logo);
+        mNames.add("KTM Youth Club");
+        mAddresses.add("Bouddha, Kathmandu");
+
+        mImages.add(R.drawable.charity_logo);
+        mNames.add("KTM Youth Club");
+        mAddresses.add("Bouddha, Kathmandu");
+
+        mImages.add(R.drawable.charity_logo);
+        mNames.add("KTM Youth Club");
+        mAddresses.add("Bouddha, Kathmandu");
+
+        mImages.add(R.drawable.charity_logo);
+        mNames.add("KTM Youth Club");
+        mAddresses.add("Bouddha, Kathmandu");
+    }
+    private void initRecyclerView(View view) {
+        Log.d(TAG, "initRecyclerView: init recyclerview");
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        RecyclerView recyclerView = view.findViewById(R.id.rvRowItem);
+        recyclerView.setLayoutManager(layoutManager);
+        RowItemAdapter adapter = new RowItemAdapter (mNames, mImages, mAddresses, getContext());
+        recyclerView.setAdapter(adapter);
     }
 
 }

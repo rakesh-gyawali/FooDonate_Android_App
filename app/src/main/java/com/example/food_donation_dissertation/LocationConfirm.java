381d8b2287e0439c70fcb3eb3b57f10e8f71b45d
Rakesh Gyawali <41475122+rakesh-gyawali@users.noreply.github.com>
2021-01-22 16:59:56 +0545

    add google map

--- app/src/main/java/com/example/food_donation_dissertation/LocationConfirm.java
+++ app/src/main/java/com/example/food_donation_dissertation/LocationConfirm.java
@@ -2,13 +2,49 @@ package com.example.food_donation_dissertation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class LocationConfirm extends AppCompatActivity {
+public class LocationConfirm extends AppCompatActivity implements View.OnClickListener {
 
+    private TextView tvMap, tvAddress;
+    private Button btnContinue;
+    private final String TAG = "LocationConfirm started";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_confirm);
+
+        tvMap = findViewById(R.id.tvMap);
+        tvAddress = findViewById(R.id.tvAddress);
+        btnContinue = findViewById(R.id.btnContinue);
+
+        tvMap.setOnClickListener(this);
+        tvAddress.setOnClickListener(this);
+        btnContinue.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.tvMap:
+                 intent = new Intent(getApplicationContext(), MapsActivity.class);
+                 startActivity(intent);
+                 break;
+            case R.id.tvAddress:
+                Log.i(TAG, "tvAddress is pressed");
+                intent = new Intent(getApplicationContext(), MapsActivity.class);
+                 startActivity(intent);
+                 break;
+            case R.id.btnContinue:
+                Log.i(TAG, "btnContinue is pressed");
+                break;
+        }
     }
 }

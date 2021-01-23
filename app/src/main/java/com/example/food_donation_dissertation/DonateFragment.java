a70aa40102790f131fae85767ddc0cc4a42ffbc6
Rakesh Gyawali <41475122+rakesh-gyawali@users.noreply.github.com>
2021-01-23 13:47:50 +0545

    added chips in donate frag

--- app/src/main/java/com/example/food_donation_dissertation/DonateFragment.java
+++ app/src/main/java/com/example/food_donation_dissertation/DonateFragment.java
@@ -7,10 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
 
 
-public class DonateFragment extends Fragment {
-
+    public class DonateFragment extends Fragment {
+    Button btnDate;
     public DonateFragment() {
         // Required empty public constructor
     }
@@ -19,12 +22,26 @@ public class DonateFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        btnDate.findViewById(R.id.btnDate);
+        MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
+        builder.setTitleText("SELECT A DATE");
+        MaterialDatePicker datePicker = builder.build();
+
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donate, container, false);
+        View view =  inflater.inflate(R.layout.fragment_donate, container, false);
+
+        return view;
     }
 }

63a5c4a0fcd54aad1935b9c0ed10a6c9da280dc5
Rakesh Gyawali <41475122+rakesh-gyawali@users.noreply.github.com>
2021-01-24 08:27:24 +0545

    added signUp and profile picture fragment

--- app/src/main/java/com/example/food_donation_dissertation/SignUpFragment.java
+++ app/src/main/java/com/example/food_donation_dissertation/SignUpFragment.java
@@ -1,14 +1,18 @@
 package com.example.food_donation_dissertation;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class SignUpFragment extends Fragment {
+    Button btnNext;
     public SignUpFragment() {
         // Required empty public constructor
     }
@@ -24,6 +28,18 @@ public class SignUpFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        btnNext = view.findViewById(R.id.btnNext);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment selectProfilePictureFragment = new SelectProfilePictureFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+                transaction.replace(R.id.frameLayout, selectProfilePictureFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
 
         return view;
     }
